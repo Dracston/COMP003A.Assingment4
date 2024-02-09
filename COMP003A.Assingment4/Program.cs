f112c90c8c1a90c679753c9a4e85545e593bab4e
Logan Jones <lpj11101@gmail.com>
2024-02-08 23:55:53 -0800

    Foo, Bar, FooBar, and int

--- COMP003A.Assingment4/Program.cs
+++ COMP003A.Assingment4/Program.cs
@@ -7,19 +7,52 @@
             Console.WriteLine("".PadRight(50, '*'));
             Console.WriteLine("for loop section \n Output a right triangle based on user input");
             Console.WriteLine("".PadRight(50, '*'));
-            Console.WriteLine("Enter a positive whole number: ") ;
-           
+            Console.WriteLine("Enter a positive whole number: ");
+
             int limit = Convert.ToInt32(Console.ReadLine());
-            string[] output = new string[] { "#"};
+            string output = "#";
+
             for (int i = 0; i < limit; i++)
             {
-               for(int symbol=1; symbol<=limit; symbol++)
-                {
-                    Console.WriteLine($"{output}");
-                }
+
+                Console.Write($"{output}");
+
                 Console.WriteLine();
+                output += "#";
 
             }
+            Console.WriteLine("".PadRight(50, '*'));
+            Console.WriteLine("foreach section \n Output all of the friends in an array");
+            Console.WriteLine("".PadRight(50, '*'));
+            string[] friends = new string[] { "Rachel", "Joey", "Chandler", "Ross", "Monica", "Phoebe" };
+            foreach (string f in friends)
+            {
+                Console.WriteLine($"\t{f}\n");
+            }
+            Console.WriteLine("".PadRight(50, '*'));
+            Console.WriteLine("while section \n Output 1-20 \n If the number is divisible by 2, output Foo \n If the number is divisible by 5, output Bar \n If the number is divisible by 2 & 5, output FooBar");
+            Console.WriteLine("".PadRight(50, '*'));
+            int counter2 = 1;
+            while (counter2 <= 20)
+            {
+                if (counter2 % 2== 0)
+                {
+                    Console.WriteLine("Foo");
+                }
+                if (counter2 % 5 == 0)
+                {
+                    Console.WriteLine("Bar");
+                }
+                if (counter2 % 2 == 0 && counter2 % 5 == 0)
+                {
+                    Console.WriteLine("FooBar");
+                }
+                else { Console.WriteLine($"{counter2}"); }
+                counter2++;
+
+            }
+
+
 
         }
     }
